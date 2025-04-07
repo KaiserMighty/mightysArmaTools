c001cb0cf78ea55ad1e82cac2228d583e0379e16
Kaiser Mighty <unikmey@gmail.com>
2025-04-07 10:52:41 -0700

    Jailbreak Radio Builder

--- radioBuilder/radioBuilder.py
+++ radioBuilder/radioBuilder.py
@@ -28,7 +28,7 @@ for file in os.listdir(MUSIC_DIR):
         base_name = os.path.splitext(file)[0]
         sanitized_name = sanitize_filename(base_name)
         output_path = os.path.join(MUSIC_DIR, f"{sanitized_name}.ogg")
-        
+
         subprocess.run([
             "ffmpeg", "-i", input_path, "-vn", "-c:a", "libvorbis", "-b:a", "64k", "-ar", "44100", output_path, "-y"
         ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
@@ -39,8 +39,8 @@ for file in os.listdir(MUSIC_DIR):
 
         os.remove(input_path)
 
-playlist_entries = '\n\t'.join([f'["{name}", {duration}]' for name, duration in song_data])
-cfg_entries = '\n\t'.join([f'class {name}\n\t{{\n\t\tsound[] = {{"121_2012_radio\\data\\music\\{name}.ogg", db+10, 1, 1000}};\n\t\ttitles[] = {{}};\n\t}};' for name, _ in song_data])
+playlist_entries = '\n\t'.join([f'["{name}", {duration}],' for name, duration in song_data])
+cfg_entries = '\n\t'.join([f'class {name}\n\t{{\n\t\tsound[] = {{"121_TACDEVRON_Radio\\data\\music\\{name}.ogg", db+10, 1, 1000}};\n\t\ttitles[] = {{}};\n\t}};' for name, _ in song_data])
 
 playlist_content = f"""
 _playlist = [
